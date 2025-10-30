5a6556732c1d7655ba071b668cf0e4beb9ef2360
Tony1406 <anthony.go0609@gmail.com>
2025-10-30 12:30:16 +0100

    segundo commit

--- nuevo-proyecto-tema2/src/app/app.ts
+++ nuevo-proyecto-tema2/src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Cabecera } from './cabecera/cabecera';
+import { Cuerpo } from './cuerpo/cuerpo';
+import { Pie } from './pie/pie';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Cabecera, Cuerpo, Pie],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('nuevo-proyecto-tema2');
+  nombre = "Ivan Noe"
 }
